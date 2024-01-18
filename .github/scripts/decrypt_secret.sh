e0144aa9b1a32ffb7deac87da8fc61e8d2abc4c1
Xusheng <xusheng@vector35.com>
2024-01-18 11:40:46 +0800

    Disable IDA plugin build and build binja plugin with dev so that the changes can be tested

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -6,7 +6,7 @@
 
 set -e
 
-gpg --quiet --batch --yes --decrypt \
-  "--passphrase=${IDASDK_SECRET}" \
-  --output "${RUNNER_WORKSPACE}/build/idasdk_teams80.zip" \
-  "${GITHUB_WORKSPACE}/ida/idasdk/idasdk_teams80.zip.gpg"
+#gpg --quiet --batch --yes --decrypt \
+#  "--passphrase=${IDASDK_SECRET}" \
+#  --output "${RUNNER_WORKSPACE}/build/idasdk_teams80.zip" \
+#  "${GITHUB_WORKSPACE}/ida/idasdk/idasdk_teams80.zip.gpg"
